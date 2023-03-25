4d099e2686e1675fd6ce76b549482f27bb112d44
Oussama Es-Sebaiy <84465604+Ousbaiy@users.noreply.github.com>
2023-03-25 15:44:35 +0300

    modified code

--- inLine.cpp
+++ inLine.cpp
@@ -3,27 +3,23 @@
 using namespace std;
 
 int main() {
-    int a = 5;
-    int b = 3;
-    int c = 0;
-    int result = 0;
-    char test = 0;
+    int i = 0;
+    int j = 0;
+
 
     __asm {
-        xor edx, edx
-        mov eax, a
-        sub eax, b; subtract b from a
-        cmp eax, 0
-        setg dl; set lower byte of eax to 1 if a - b > 0
-        cmp c, 0; compare c with 0
-        seten test
-        and dl, test
-        mov result, edx
-
-
+        mov eax, i
+        mov ebx, j
+        loop_header:
+        cmp eax, 5      ; compare i with 5
+        jge loop_end    ; jump tp loop_end if i >= 5
+        add ebx, eax    ; add i to j
+        inc eax         ; increment i
+        jmp loop_header ; jump back to loop_header
+        loop_end:
+        
     }
 
-    cout << "Result: " << result << endl;
 
     return 0;
 }
