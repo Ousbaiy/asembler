b41409cd9d1cb2212a579bff1a0b691a5e4cbec5
Oussama Es-Sebaiy <84465604+Ousbaiy@users.noreply.github.com>
2023-04-05 21:40:46 +0300

    added floating points

--- inLine.cpp
+++ inLine.cpp
@@ -2,24 +2,34 @@
 
 using namespace std;
 
+float calculate(float a, float b, float c, float d, float e, float f);
+
 int main() {
-    int i = 0;
-    int j = 0;
+
+    float a = 1;
+    float b = 2;
+    float c = 3;
+    float d = 2;
+    float e = 1;
+    float f = 4;
 
 
-    __asm {
-        mov eax, i
-        mov ebx, j
-        loop_header:
-        cmp eax, 5      ; compare i with 5
-        jge loop_end    ; jump tp loop_end if i >= 5
-        add ebx, eax    ; add i to j
-        inc eax         ; increment i
-        jmp loop_header ; jump back to loop_header
-        loop_end:
-        
-    }
-
-
+    cout << "My results: " << calculate( a,  b,  c,  d,  e,  f) << endl;
     return 0;
+}
+
+float calculate(float a, float b, float c, float d, float e, float f) {
+    float result;
+    __asm {
+        finit
+        fld b
+        fmul c
+        fadd a
+        fld e
+        fmul f
+        fsubr d
+        fdivp st(1), st(0)
+        fst result 
+    }
+    return result;
 }
